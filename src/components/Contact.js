d14fde95a87f975b30d1a8de3c811d6107ebb1c6
Fadeltnt <101524603+Fadeltnt@users.noreply.github.com>
2024-09-18 18:46:01 -0400

    some bug fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,3 @@
-// src/components/Contact.js
-
 import React from "react";
 
 export default function Contact() {
@@ -55,6 +53,7 @@ export default function Contact() {
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                                 EMAIL
                             </h2>
+                            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                             <a className="text-indigo-400 leading-relaxed">
                                 fadeltinto@gmail.com
                             </a>
@@ -66,8 +65,10 @@ export default function Contact() {
                     </div>
                 </div>
                 <form
-                    netlify
                     name="contact"
+                    method="POST"
+                    data-netlify="true"
+                    netlify-honeypot="bot-field"
                     onSubmit={handleSubmit}
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
@@ -77,6 +78,10 @@ export default function Contact() {
                         Got a question or proposal, or just want
                         to say hello? Go ahead.
                     </p>
+                    <div hidden>
+                        <input type="hidden" name="form-name" value="contact" />
+                        <input type="hidden" name="bot-field" />
+                    </div>
                     <div className="relative mb-4">
                         <label htmlFor="name" className="leading-7 text-sm text-gray-400">
                             Name
@@ -102,9 +107,7 @@ export default function Contact() {
                         />
                     </div>
                     <div className="relative mb-4">
-                        <label
-                            htmlFor="message"
-                            className="leading-7 text-sm text-gray-400">
+                        <label htmlFor="message" className="leading-7 text-sm text-gray-400">
                             Message
                         </label>
                         <textarea
