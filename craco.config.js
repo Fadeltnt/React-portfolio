a7f63d9a0b5b12764c0270b8efd69a0eec3741b7
Fadeltnt <101524603+Fadeltnt@users.noreply.github.com>
2024-09-18 09:20:51 -0400

    Deployment ready

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,32 @@
+const path = require('path');
+
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
+  webpack: {
+    configure: (webpackConfig) => {
+      const babelLoader = {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        include: [
+          path.resolve(__dirname, 'src'),
+          path.resolve(__dirname, 'node_modules/react-icons'),
+        ],
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+          ],
+          plugins: [
+            '@babel/plugin-transform-runtime',
+          ],
+          cacheDirectory: true,
+          cacheCompression: false,
+          compact: false,
+        },
+      };
+
+      webpackConfig.module.rules.push(babelLoader);
+
+      return webpackConfig;
     },
   },
-}
+};
