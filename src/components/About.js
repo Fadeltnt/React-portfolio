c0368c86fc114548a9fac587cdc94ff1fe2ce876
Fadeltnt <101524603+Fadeltnt@users.noreply.github.com>
2024-09-18 10:12:30 -0400

    some bug fix

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,16 @@ export default function About() {
     return (
         <section id="about">
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                <div
+                    className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                         Hi, I'm Fadel
-                        <br className="hidden lg:inline-block"/> Software Engineering student.
+                        <br className="hidden lg:inline-block"/> Software Engineer student.
                     </h1>
                     <p className="mb-8 leading-relaxed">
-                        I’m a third-year student at Université Laval, a nerd with a love for manga and video games, and I’m passionate about web development.I enjoy crafting elegant and efficient solutions to complex problems. Explore my work to discover more about my skills and achievements.
+                        I’m a third-year student at Université Laval, a nerd with a love for manga and video games, and
+                        I’m passionate about web development.I enjoy crafting elegant and efficient solutions to complex
+                        problems. Explore my work to discover more about my skills and achievements.
                     </p>
                     <div className="flex justify-center">
                         <a
@@ -28,7 +31,7 @@ export default function About() {
                 </div>
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                     <img
-                        className="object-cover object-center rounded"
+                        className="object-cover object-center rounded hidden md:block"
                         alt="hero"
                         src="/IMG_9182.jpg"
                     />
